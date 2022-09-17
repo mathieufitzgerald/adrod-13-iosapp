c9a2f3ae44470d86144ef33595c5ba360dd6db4a
matt <63452179+mathieufitzgerald@users.noreply.github.com>
2022-09-17 23:44:44 +0200

    initial upload

---
+++ adrod13/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  freeram2022
+//
+//  Created by Mathieu Fitzgerald on 16.09.22.
+//
+
+import UIKit
+import AVFoundation
+var audioPlayer = AVAudioPlayer()
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var MySlider: UISlider!
+    @IBOutlet weak var MyLabel: UILabel!
+    @IBOutlet weak var MyButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let sound = Bundle.main.path(forResource: "defaultsound", ofType: "mp3")
+        
+        do {
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch{
+            
+            print(error)
+        }
+        audioPlayer.play()
+        
+        
+
+    }
+
+    
+    
+    
+    }
+
+        
+
+    
+
+
+
